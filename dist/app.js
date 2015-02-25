c28a18b7f1068109262acf933dc042ccec595346
pol <alejonext@gmail.com>
2015-02-24 23:39:23 -0500

    dist

--- dist/app.js
+++ dist/app.js
@@ -42685,7 +42685,7 @@ module.exports = function (scope, tumblr) {
 	scope.reload = function () {
 		scope.render.page++;
 		tumblr.posts(scope.render).$promise.then(function (resp) {
-			if(angular.isObject(resp.response) && angular.isArray(resp.response.posts) && resp.response.total_posts > resp.response.posts.length){
+			if(angular.isObject(resp.response) && angular.isArray(resp.response.posts) && resp.response.total_posts > scope.posts.length){
 				for (var i = resp.response.posts.length - 1; i >= 0; i--)
 					scope.posts.push(resp.response.posts[i]);
 			}
